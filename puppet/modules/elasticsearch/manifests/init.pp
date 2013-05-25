b613976876441011aeb249414c36cfe66a8f2f22
Mathew Davies <thepixeldeveloper@googlemail.com>
2013-05-25 15:26:30 +0100

    Add puppet module for elasticsearch.

---
+++ puppet/modules/elasticsearch/manifests/init.pp
@@ -0,0 +1,28 @@
+class elasticsearch {
+  $pkg  = "elasticsearch"
+  $ver  = "0.90.0"
+  $full = "${$pkg}-${$ver}"
+
+  exec { "download-elasticsearch":
+    command => "wget http://download.elasticsearch.org/$pkg/$pkg/$full.deb -O /tmp/$full.deb --no-check-certificate",
+    unless => "test -f /tmp/$full.deb"
+  }
+
+  package { "elasticsearch":
+    provider => dpkg,
+    ensure   => latest,
+    source   => "/tmp/$full.deb",
+    require  => [
+      Package["default-jre"],
+      Exec["download-elasticsearch"],
+    ]
+  }
+
+  package { "default-jre": }
+
+  service { "elasticsearch":
+    enable  => true,
+    ensure  => running,
+    require => Package["elasticsearch"]
+  }
+}
