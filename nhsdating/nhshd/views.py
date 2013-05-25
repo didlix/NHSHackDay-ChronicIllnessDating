cf7de0b2db5fbe553c4b269cbae7714cf74c2296
Alex Stapleton <alexs@prol.etari.at>
2013-05-25 16:09:46 +0100

    Send patient to template

--- nhsdating/nhshd/views.py
+++ nhsdating/nhshd/views.py
@@ -1,15 +1,22 @@
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
+from django.shortcuts import get_object_or_404
+
+
+from models import Patient
 
 def home(request):
     return TemplateResponse(request, 'home.html', {})
 
 
-def profile(request, user_name):
+def profile(request, username):
     """
     Users profile page
     """
-    return TemplateResponse(request, 'profile.html', {})
+
+    patient = get_object_or_404(Patient, user__username=username)
+
+    return TemplateResponse(request, 'profile.html', {"patient": patient})
 
 
 def matches(request):
