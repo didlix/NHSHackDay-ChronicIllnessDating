f90e37e37e908cbf65940a3f6754546a168c9164
Nick Smith <nick@nicksmith.org>
2013-05-25 14:37:23 +0100

    register models on admin site

--- nhsdating/urls.py
+++ nhsdating/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^(?P<user_name>[^/]+)$', 'nhshd.views.profile', name='profile'),
     url(r'^$', 'nhshd.views.home', name='home'),
-    url(r'^admin/', include(admin.site.urls)),
 )
