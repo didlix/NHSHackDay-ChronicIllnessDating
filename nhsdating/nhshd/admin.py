7f812b5e6d80cd370c33bb803df8cb5a077e8656
d0c0nnor <dannyoc@gmail.com>
2013-05-25 16:10:51 +0100

    Added admin fields and migration

--- nhsdating/nhshd/admin.py
+++ nhsdating/nhshd/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from nhshd.models import Patient, Place, Interest, Symptom, Photo
+from nhshd.models import Patient, Place, Interest, Symptom, Photo, HealthcareLocation
 
 admin.site.register(Patient)
 admin.site.register(Place)
 admin.site.register(Interest)
 admin.site.register(Symptom)
 admin.site.register(Photo)
+admin.site.register(HealthcareLocation)
