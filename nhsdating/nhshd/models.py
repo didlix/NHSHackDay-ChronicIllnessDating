70f449b18e8b5d41f4c593bc5015cd0f688bdf32
Alex Stapleton <alexs@prol.etari.at>
2013-05-25 11:57:15 +0100

    Photos model with actual fields

--- nhsdating/nhshd/models.py
+++ nhsdating/nhshd/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 
+def format_filename(format_string):
+    """
+    Takes a Django format string like
+
+        "/a/path/{instance.lol}/"
+
+    and returns a function for the FileField
+    """
+    def upload_to(instance, filename):
+        return format_string.format(obj=instance, filename=filename})
+
+
 class Patient(models.Model):
     pass
 
@@ -14,6 +26,5 @@ class Symptom(models.Model):
 
 
 class Photo(models.Model):
-    pass
-
-
+    created_by = models.ForeignKey('contrib.auth.models.User')
+    image = models.ImageField(upload_to=format_filename("/photos/{filename}"))
