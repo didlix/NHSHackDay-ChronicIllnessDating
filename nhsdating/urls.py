65cac8f7b8f114c441f77a353432141ef471be6b
Mathew Davies <thepixeldeveloper@googlemail.com>
2013-05-25 14:42:51 +0100

    Add search routes.

--- nhsdating/urls.py
+++ nhsdating/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns(
     url(r'^(?P<user_name>[^/]+)$', 'nhshd.views.profile', name='profile'),
     url(r'^$', 'nhshd.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^search/', include('haystack.urls')),
 )
