ba36fb8eec76633e4e7112f9e049dddfd640ecda
Mathew Davies <thepixeldeveloper@googlemail.com>
2013-05-25 15:00:11 +0100

    Update the haystack URL fields.

--- nhsdating/urls.py
+++ nhsdating/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^search', include('haystack.urls')),
     url(r'^(?P<user_name>[^/]+)$', 'nhshd.views.profile', name='profile'),
     url(r'^$', 'nhshd.views.home', name='home'),
-    url(r'^search/', include('haystack.urls')),
 )
