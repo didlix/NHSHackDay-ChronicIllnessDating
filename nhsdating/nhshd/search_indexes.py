f45d3e0af4f4307bc10d87a982076ddb76de2937
Mathew Davies <thepixeldeveloper@googlemail.com>
2013-05-25 17:03:25 +0100

    Lets keep the index simple for now.

--- nhsdating/nhshd/search_indexes.py
+++ nhsdating/nhshd/search_indexes.py
@@ -2,16 +2,9 @@ import datetime
 from haystack import indexes
 from nhshd.models import Patient
 
-class NHSHD(indexes.SearchIndex, indexes.Indexable):
+class nhshd(indexes.SearchIndex, indexes.Indexable):
     text            = indexes.CharField(document=True, use_template=True)
     name            = indexes.CharField(model_attr='name')
-    gender          = indexes.CharField(model_attr='gender')
-    age             = indexes.CharField(model_attr='age')
-    locations       = indexes.CharField(model_attr='locations')
-    symptoms        = indexes.CharField(model_attr='symptoms')
-    interests       = indexes.CharField(model_attr='interests')
-    personal_words  = indexes.CharField(model_attr='personal_words')
-    favourite_words = indexes.CharField(model_attr='favourite_words')
     
     def get_model(self):
         return Patient
