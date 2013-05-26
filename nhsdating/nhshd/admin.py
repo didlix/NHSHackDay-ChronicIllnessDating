ab46a8a45451649ae739559a85e9038d8f573962
Alex Stapleton <alexs@prol.etari.at>
2013-05-26 10:19:14 +0100

    Add message model to admin

--- nhsdating/nhshd/admin.py
+++ nhsdating/nhshd/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from nhshd.models import Patient, Place, Interest, Symptom, Photo, HealthcareLocation, Condition
+from nhshd.models import Patient, Place, Interest, Symptom, Photo, HealthcareLocation, Condition, Message
 
 admin.site.register(Patient)
 admin.site.register(Place)
 admin.site.register(Interest)
 admin.site.register(Symptom)
 admin.site.register(Photo)
+admin.site.register(Message)
 admin.site.register(HealthcareLocation)
 admin.site.register(Condition)
