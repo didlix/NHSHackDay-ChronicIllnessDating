608dd7cf508e61405feedafe60a2eb0c481da600
Alex Stapleton <alexs@prol.etari.at>
2013-05-26 13:38:14 +0100

    some sort of "algorithm"

--- nhsdating/nhshd/matcher.py
+++ nhsdating/nhshd/matcher.py
@@ -1,5 +1,18 @@
 from .models import Patient
 
-def generate_matches(interests, conditions, symptoms, age_from, age_to,locations):
+def generate_matches(interests, conditions, symptoms, age_from, age_to, locations):
     matches = Patient.objects.all()
-    return matches
+
+    def sort_key(p):
+        c = p.other_conditions.filter(name__in=conditions).count() if conditions else None
+        s = p.symptoms.filter(name__in=symptoms).count() if symptoms else None
+        i = p.interests.filter(name__in=interests).count() if interests else None
+
+        return (
+            p.age >= age_from and p.age <= age_to,
+            c,
+            s,
+            i
+        )
+
+    return sorted(matches, key=sort_key)
